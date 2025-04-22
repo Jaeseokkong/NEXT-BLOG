ec6deb283096adee1ce9ae32b52ceab2cb844986
JaeSeok <wotjr5124@gmail.com>
2025-04-22 22:56:36 +0900

    feat: 카테고리 및 작성글(Markdown) fetch 추가

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -10,3 +10,16 @@ export async function fetchCategories(): Promise<string[]> {
   const data = await res.json();
   return data.filter((item: any) => item.type === "dir" ).map((item: any) => item.name);
 }
+
+export async function fetchFilesInCategory(category: string): Promise<{name : string; path: string}[]> {
+  const res = await fetch(`${BASE_URL}/${category}`, { next: { revalidate: 3600 }});
+  const data = await res.json();
+  console.log(data.filter((item: any) => item.name.endsWith(".md")).map((item: any) => ({ name: item.name, path: item.path})))
+  return data.filter((item: any) => item.name.endsWith(".md")).map((item: any) => ({ name: item.name, path: item.path}))
+}
+
+export async function fetchMarkdownFile(path: string): Promise<string> {
+  const res = await fetch(`https://raw.githubusercontent.com/Jaeseokkong/TIL/main/${path}`);
+  const data = res.json();
+  return await res.text();
+}
