2e37c25c41b2debf51cd2cd83e1213e0031bec53
JaeSeok <wotjr5124@gmail.com>
2025-05-09 21:56:34 +0900

    fix: postlist

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -10,20 +10,21 @@ interface Post {
 
 const PostList = ({ posts }: { posts: Post[] }) => {
   return (
-    <ul className="space-y-6">
-      {posts.map((post) => (
-        <li key={`${post.category}/${post.slug}`} className="border-b pb-4">
-          <Link href={`/post/${post.category}/${post.slug}`}>
-            <h2 className="text-xl font-semibold hober:underline">{post.title}</h2>
-          </Link>
-          <p className="text-sm text-zinc-500">
-            {post.excerpt && <p className="text-zinc-700 mt-2">{post.excerpt}</p>}
-          </p>
-        </li>
-      ))
-
-      }
-    </ul>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+    {posts.map((post) => (
+      <Link
+        key={`${post.category}/${post.slug}`}
+        href={`/post/${post.category}/${post.slug}`}
+        className="block p-4 border rounded-2xl shadow-md hover:shadow-lg transition bg-white dark:bg-zinc-900"
+      >
+        <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
+        <p className="text-sm text-gray-500 dark:text-gray-400 mb-1">
+          📅 {post.date} | 📂 {post.category}
+        </p>
+        <p className="text-gray-700 dark:text-gray-300 text-sm">{post.excerpt}</p>
+      </Link>
+    ))}
+  </div>
   )
 }
 
