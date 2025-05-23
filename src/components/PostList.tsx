02c52f2923a5899cb5d9492c559acaefd4d412e1
JaeSeok <wotjr5124@gmail.com>
2025-05-24 07:35:46 +0900

    style: PostList

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -10,20 +10,25 @@ interface Post {
 
 const PostList = ({ posts }: { posts: Post[] }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-    {posts.map((post) => (
-      <Link
-        key={`${post.category}/${post.slug}`}
-        href={`/post/${post.category}/${post.slug}`}
-        className="block p-4 border rounded-2xl shadow-md hover:shadow-lg transition bg-white dark:bg-zinc-900"
-      >
-        <h3 className="text-xl font-semibold mb-2">{post.title}</h3>
-        <p className="text-sm text-gray-500 dark:text-gray-400 mb-1">
-          📅 {post.date} | 📂 {post.category}
-        </p>
-        <p className="text-gray-700 dark:text-gray-300 text-sm">{post.excerpt}</p>
-      </Link>
-    ))}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {posts.map((post) => (
+        <Link
+          key={`${post.category}/${post.slug}`}
+          href={`/post/${post.category}/${post.slug}`}
+          className="block rounded-2xl shadow-md hover:shadow-lg transition bg-white dark:bg-zinc-900"
+        >
+          <div className='flex-1 h-[150px] bg-amber-200 rounded-t-2xl flex'>
+          </div>
+          <div className="py-2 px-4">
+            <h3 className="text-sm font-semibold mb-2">{post.title}</h3>
+            <p className="text-sm text-gray-500 dark:text-gray-400 mb-1">
+              📅 {post.date} | 📂 {post.category}
+            </p>
+            <p className="text-gray-700 dark:text-gray-300 text-sm">{post.excerpt}</p>
+          </div>
+          
+        </Link>
+      ))}
   </div>
   )
 }
