726c66886306f133048087b6ba10553ebb5c6780
JaeSeok <wotjr5124@gmail.com>
2025-05-23 22:18:21 +0900

    fix: main 레이아웃=

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <Header />
       <div className="flex">
         {/* <Sidebar/> */}
-        <main className="flex-1 overflow-auto max-w-7xl mx-auto mt-5">
+        <main className="flex-1 overflow-auto max-w-7xl mx-auto py-10 px-6">
           <h1 className="text-3xl font-bold mb-6">📌 최신 포스트</h1>
           <PostList posts={posts} />
         </main>
