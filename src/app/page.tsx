2d62d40b42e4e229f9906be529fb193e41b0ec2f
JaeSeok <wotjr5124@gmail.com>
2025-05-26 22:17:39 +0900

    fix: 메인 페이지 재구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,14 @@ export default async function Home() {
   return (
     <>
       <Header />
-      <div className="flex">
-        {/* <Sidebar/> */}
-        <main className="flex-1 overflow-auto max-w-7xl mx-auto py-10 px-6">
-          <h1 className="text-3xl font-bold mb-6">📌 최신 포스트</h1>
-          <PostList posts={posts} />
-        </main>
+      <div className="flex py-10">
+        <div className="max-w-7xl mx-auto flex flex-row gap-10">
+          <Sidebar/>
+          <main className="flex-1 overflow-auto px-6">
+            <h1 className="text-3xl font-bold mb-6">📌 최신 포스트</h1>
+            <PostList posts={posts} />
+          </main>
+        </div>
       </div>
     </>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,12 @@
-'use client'
-import { fetchCategories } from '@/lib/github';
+import { fetchCategories, PostItemType } from '@/lib/github';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
-const Sidebar = () => {
-  const [categories, setCategories] = useState<string[]>();
-
-  console.log(categories)
-  useEffect(() => {
-    fetchCategories().then(setCategories).catch(console.error);
-  }, [])
+const Sidebar = async () => {
+  const categories = await fetchCategories();
 
   return (
-    <aside className="w-60 max-h-[80vh] p-6 bg-zinc-50 dark:bg-zinc-800 rounded-xl shadow-md overflow-y-auto">
+    <aside className="w-60 p-6 bg-zinc-50 dark:bg-zinc-800 rounded-xl shadow-md overflow-y-auto h-fit">
       <h2 className="text-2xl font-semibold text-zinc-700 dark:text-zinc-200 mb-6">📚 카테고리</h2>
       <ul className="space-y-3">
         {categories?.map((category) => (
