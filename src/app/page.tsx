70cb208b6df7fe6eadef029cf4fc0e18dd849e4d
JaeSeok <wotjr5124@gmail.com>
2025-06-02 22:16:09 +0900

    fix: sidebar, main grid 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default async function Home() {
 
   return (
     <>
-      <div className="flex py-10">
-        <div className="max-w-7xl mx-auto flex flex-row gap-10">
+      <div className="py-10">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[250px_1fr] gap-10">
           <Sidebar/>
-          <main className="flex-1 overflow-auto px-6">
+          <main className="flex-1 overflow-auto">
             <Intro />
             <h1 className="text-3xl font-bold mb-6">📌 최신 포스트</h1>
             <PostList />
