cc59c15097a5bfc4d9e139b76899d800d2885255
JaeSeok <wotjr5124@gmail.com>
2025-06-03 23:06:41 +0900

    fix: PostCard 컴포넌트 - key 삭제

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 
-
 interface Props {
-  key: string;
   post: Post;
 }
 
@@ -14,24 +12,24 @@ interface Post {
   excerpt?: string;
 }
 
-const PostCard = ({ key, post }: Props) => {
+const PostCard = ({ post }: Props) => {
   return (
     <Link
-        key={key}
-        href={`/post/${post.category}/${post.slug}`}
-        className="block rounded-2xl shadow-md hover:shadow-lg transition bg-white dark:bg-zinc-900"
-      >
-        <div className='flex-1 h-[150px] bg-amber-200 rounded-t-2xl flex'>
-        </div>
-        <div className="py-2 px-4">
-          <h3 className="text-sm font-semibold mb-2">{post.title}</h3>
-          <p className="text-sm text-gray-500 dark:text-gray-400 mb-1">
-            📅 {post.date} | 📂 {post.category}
-          </p>
-          <p className="text-gray-700 dark:text-gray-300 text-sm">{post.excerpt}</p>
-        </div>
+      href={`/post/${post.category}/${post.slug}`}
+      className="block rounded-2xl shadow-md hover:shadow-lg transition bg-white dark:bg-zinc-900"
+    >
+      <div className="flex-1 h-[150px] bg-amber-200 rounded-t-2xl flex"></div>
+      <div className="py-2 px-4">
+        <h3 className="text-sm font-semibold mb-2">{post.title}</h3>
+        <p className="text-sm text-gray-500 dark:text-gray-400 mb-1">
+          📅 {post.date} | 📂 {post.category}
+        </p>
+        <p className="text-gray-700 dark:text-gray-300 text-sm">
+          {post.excerpt}
+        </p>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
