f27d54b7d73d75e66bbc861cf68cd64db13371b6
jaeseok <wotjr5124@gmail.com>
2025-07-01 15:53:22 +0900

    feat: 메인 소개 미리보기 추가 - sidebar 제거 (추후 posts 넣을예정) - AboutPreview 메인 레이아웃 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import AboutPreview from "@/components/AboutPreview";
 import Intro from "@/components/Intro";
 import PostList from "@/components/PostList";
-import Sidebar from "@/components/Sidebar";
 import { fetchPostMetas } from "@/lib/github";
 
 export default async function Home() {
@@ -8,10 +8,10 @@ export default async function Home() {
 
   return (
     <div className="py-10 px-4">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[250px_1fr] gap-10">
-        <Sidebar />
+      <div className="max-w-7xl mx-auto grid grid-cols-1 gap-10">        
         <main className="overflow-auto">
           <Intro />
+          <AboutPreview />
           <h1 className="text-3xl font-bold mb-6">📌 최신 포스트</h1>
           <PostList initialPosts={initialPosts} />
         </main>
