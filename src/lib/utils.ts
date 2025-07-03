d006689d6a353290322df880d642e41ff57f6314
jaeseok <wotjr5124@gmail.com>
2025-07-03 10:52:04 +0900

    feat: formatDate 함수 분리

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,3 +23,18 @@ export function extractFirstImage(markdown: string): string | null {
   }
   return null;
 }
+
+export function formatDate(dateString: string): string {
+  const d = new Date(
+    dateString.slice(0, 4) +
+      '-' +
+      dateString.slice(4, 6) +
+      '-' +
+      dateString.slice(6)
+  );
+  return d.toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
