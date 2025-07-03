05be62a7bdd1126199825308cf6b242687bc973f
jaeseok <wotjr5124@gmail.com>
2025-07-03 10:53:19 +0900

    fix: 타입 이름 수정 - TechBoxProps

--- src/components/ui/techBox.tsx
+++ src/components/ui/techBox.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-interface techBoxProps {
+interface TechBoxProps {
   techName: string;
 }
 
-export default function TechBox({ techName }: techBoxProps) {
+export default function TechBox({ techName }: TechBoxProps) {
   return (
-    <span className="px-2 py-1 bg-zinc-200 dark:bg-zinc-700 rounded">{techName}</span>
+    <span className="px-2 py-1 bg-zinc-200 dark:bg-zinc-700 rounded" key={techName} >{techName}</span>
   )
 }
