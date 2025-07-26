09a7561173ca8ae76faa7d0a99f16287942d065b
JaeSeok <wotjr5124@gmail.com>
2025-07-26 21:39:28 +0900

    feat: giscus 컨테이너 삭제 - iframe 적용하여 컨테이너와 상관없이 스타일 적용되지 않음

--- src/app/posts/[category]/[slug]/page.tsx
+++ src/app/posts/[category]/[slug]/page.tsx
@@ -21,31 +21,29 @@ export default async function PostPage({ params }: Props) {
   const markdown = await fetchMarkdownFile(category, slug);
 	
 	return (
-    <div className="markdown prose prose-neutral max-w-3xl mx-auto dark:prose-invert prose-headings:scroll-mt-24 prose-h2:mt-12 prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900 py-10">
+    <div className="markdown prose prose-neutral max-w-4xl mx-auto dark:prose-invert prose-headings:scroll-mt-24 prose-h2:mt-12 prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900 py-10 px-4 lg:px-0">
 			<ReactMarkdown
 				remarkPlugins={[remarkGfm]}
 				rehypePlugins={[rehypeRaw, rehypeHighlight]}
 			>
 				{markdown}
 			</ReactMarkdown>
-			<div id="comments" style={{ marginTop: '3rem' }} className="mx-10">
-				<Script
-					src="https://giscus.app/client.js"
-					data-repo="Jaeseokkong/BLOG-COMMENTS"
-					data-repo-id="R_kgDOPSpQbg"
-					data-category="General"
-					data-category-id="DIC_kwDOPSpQbs4CtZ8u"
-					data-mapping="pathname"
-					data-strict="0"
-					data-reactions-enabled="1"
-					data-emit-metadata="0"
-					data-input-position="bottom"
-					data-theme="preferred_color_scheme"
-					data-lang="ko"
-					crossOrigin="anonymous"
-					strategy="lazyOnload"
-				/>
-			</div>
+			<Script
+				src="https://giscus.app/client.js"
+				data-repo="Jaeseokkong/BLOG-COMMENTS"
+				data-repo-id="R_kgDOPSpQbg"
+				data-category="General"
+				data-category-id="DIC_kwDOPSpQbs4CtZ8u"
+				data-mapping="pathname"
+				data-strict="0"
+				data-reactions-enabled="1"
+				data-emit-metadata="0"
+				data-input-position="bottom"
+				data-theme="preferred_color_scheme"
+				data-lang="ko"
+				crossOrigin="anonymous"
+				strategy="lazyOnload"
+			/>
 		</div>
 	)
 }
