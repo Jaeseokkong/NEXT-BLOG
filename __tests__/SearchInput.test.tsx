69cbedd744d8eef5f477e40de4da27182b0a41c3
jaeseok <wotjr5124@gmail.com>
2025-09-17 16:35:56 +0900

    test: debounce 검색 waitFor로 검증

--- __tests__/SearchInput.test.tsx
+++ __tests__/SearchInput.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import SearchInput from '@/components/posts/SearchInput';
 
-describe('SearchInput', () => {
+describe('<SearchInput/>', () => {
   const placeholderText = "검색어를 입력하세요...";
   let onSearchMock: jest.Mock;
 
@@ -15,12 +15,16 @@ describe('SearchInput', () => {
     expect(input.value).toBe('');
   });
 
-  it('입력하면 onSearch가 호출된다', () => {
+  it('입력하면 onSearch가 호출된다', async () => {
     const input = screen.getByPlaceholderText(placeholderText) as HTMLInputElement;
 
     fireEvent.change(input, { target: { value: 'Jest' } });
-    expect(input.value).toBe('Jest');
-    expect(onSearchMock).toHaveBeenCalledWith('Jest');
+
+    await waitFor(() => {
+      expect(onSearchMock).toHaveBeenCalledWith('Jest');
+    })
+    
+    expect(input.value).toBe('Jest')
   });
 
   it('X 버튼 클릭하면 값이 지워지고 onSearch가 빈 문자열로 호출된다', () => {
