0740a0f6ecea2c0a3729aa6c695d82aa90a5393c
JaeSeok <wotjr5124@gmail.com>
2025-09-26 21:35:56 +0900

    test: ProjectsPreview 프리뷰 4개 렌더링 테스트

--- __tests__/ProjectsPreview.test.tsx
+++ __tests__/ProjectsPreview.test.tsx
@@ -1,6 +1,48 @@
+import ProjectsPreview from "@/components/home/ProjectsPreview"
+import { getAllProjects } from "@/lib/projects";
+import { render, screen } from "@testing-library/react";
+
+jest.mock('@/lib/projects', () => ({
+	getAllProjects: jest.fn().mockReturnValue([
+		{
+			title: '프로젝트 타이틀1',
+			description: '프로젝트 내용1',
+			tech: ["기술1", "기술2", "기술3"]
+		},
+		{
+			title: '프로젝트 타이틀2',
+			description: '프로젝트 내용2',
+			tech: ["기술1", "기술2", "기술3"]
+		},
+		{
+			title: '프로젝트 타이틀3',
+			description: '프로젝트 내용3',
+			tech: ["기술1", "기술2", "기술3"]
+		},
+		{
+			title: '프로젝트 타이틀4',
+			description: '프로젝트 내용4',
+			tech: ["기술1", "기술2", "기술3"]
+		},
+		{
+			title: '프로젝트 타이틀5',
+			description: '프로젝트 내용5',
+			tech: ["기술1", "기술2", "기술3"]
+		}
+	])
+}))
 
 describe('<ProjectPreview />', () => {
-	it('ProjectPreview 올바르게 렌더링', () => {
-		
+	
+
+	
+
+	beforeEach(() => {
+		render(<ProjectsPreview />);
+	})
+
+	it('프로젝트 프리뷰가 4개만 렌더링된다.', () => {
+		const projects = screen.getAllByRole('heading', { level: 3 });
+		expect(projects).toHaveLength(4);
 	})
 })
