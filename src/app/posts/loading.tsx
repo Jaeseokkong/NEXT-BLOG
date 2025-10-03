63fe47aba833c8aa15ed3b7787e2be56567d194a
jaeseok <wotjr5124@gmail.com>
2025-10-03 19:49:54 +0900

    feat: 포스트 라이트 진입 시 데이터 기다리는 동안 보여질 로딩 추가

---
+++ src/app/posts/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-64">
+      <div className="w-12 h-12 border-4 border-zinc-300 dark:border-zinc-600 border-t-yellow-500 dark:border-t-yellow-400 rounded-full animate-spin"></div>
+    </div>
+  );
+}
