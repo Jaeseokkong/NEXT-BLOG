e40b5cad4bd2f1f14ac56662510bc755929f6e33
JaeSeok <wotjr5124@gmail.com>
2026-01-06 21:37:36 +0900

    fix(Heading): level props 타입 명시

--- src/components/atoms/Heading.tsx
+++ src/components/atoms/Heading.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
+type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+
 interface HeadingProps {
-    level: number;
+    level: HeadingLevel;
     children: React.ReactElement;
 }
 
