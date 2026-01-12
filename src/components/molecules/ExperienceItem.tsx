11bfd9ecc100ca172210639b2584b336a1e7ea13
jaeseok <wotjr5124@gmail.com>
2026-01-12 16:20:50 +0900

    feat(molecules): ExperienceItem 컴포넌트 추가

--- src/components/molecules/ExperienceItem.tsx
+++ src/components/molecules/ExperienceItem.tsx
@@ -1,15 +1,33 @@
+import Text from "../atoms/Text";
+import Title from "../atoms/Title";
+
 interface ExperienceItemProps {
 	company: string[];
 	role: string;
 	period: string;
-	description: string[];
+	descriptions: string[];
 }
 
 
-export default function ExperienceItem({ company, role, period, description }: ExperienceItemProps) {
+export default function ExperienceItem({ company, role, period, descriptions }: ExperienceItemProps) {
 	return (
 		<div className="space-y-2">
-			
+			<div className="flex items-baseline justify-between">
+				<Title level={3}>{company}</Title>
+				<Text size="sm" className="text-gray-500">
+					{period}
+				</Text>
+				<Text size="sm" className="text-gray-600">
+					{role}
+				</Text>
+				<ul className="list-disc pl-4 space-y-1">
+					{descriptions.map((desc, idx) => (
+					<li key={idx}>
+						<Text size="sm">{desc}</Text>
+					</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	)
 }
