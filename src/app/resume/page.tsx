bd2cf5ab3c2460ff558eda1238d13d2c0f2c1f19
JaeSeok <wotjr5124@gmail.com>
2026-01-16 21:45:01 +0900

    feat(resume): Link target 속성 지정

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,3 +1,4 @@
+import Divider from "@/components/atoms/Divider";
 import ResumeHeader from "./components/organisms/ResumeHeader";
 import ResumeTemplate from "./components/templates/ResumeTemplate";
 
@@ -5,6 +6,7 @@ export default function ResumePage() {
 	return (
 		<ResumeTemplate>
 			<ResumeHeader/>
+			<Divider variant="section" />
 		</ResumeTemplate>
 	)
 }
