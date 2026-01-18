a628b618869b338deb0a2008b1567587e802c305
JaeSeok <wotjr5124@gmail.com>
2026-01-18 19:57:10 +0900

    feat(resume): ExperienceSection 추가

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,13 +1,14 @@
-import Divider from "@/components/atoms/Divider";
 import ResumeHeader from "./components/organisms/ResumeHeader";
 import ResumeTemplate from "./components/templates/ResumeTemplate";
 import SkillSection from "./components/organisms/SkillSection";
+import ExperienceSection from "./components/organisms/ExperienceSection";
 
 export default function ResumePage() {
 	return (
 		<ResumeTemplate>
 			<ResumeHeader />
 			<SkillSection />
+			<ExperienceSection />
 		</ResumeTemplate>
 	)
 }
