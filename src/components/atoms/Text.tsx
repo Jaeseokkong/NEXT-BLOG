cd20dbc89e68d76e06010594366fb90c3b57d4a5
JaeSeok <wotjr5124@gmail.com>
2026-02-01 22:04:44 +0900

    feat(atoms): Text props as a 태그 및 href 추가

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -2,12 +2,13 @@ import React, { JSX } from 'react'
 
 interface TextProps {
 	children: React.ReactNode;
-	as?: 'p' | 'span' | 'div';
+	as?: 'p' | 'span' | 'div' | 'a';
 	size?: 'sm' | 'base' | 'lg';
+	href?: string;
 	className?: string;
 }
 
-export default function Text({ children, as = 'p', size = 'base', className }: TextProps) {
+export default function Text({ children, as = 'p', size = 'base', href, className }: TextProps) {
 	const Tag = as as keyof JSX.IntrinsicElements;
 
 	const sizeStyles = {
@@ -18,10 +19,12 @@ export default function Text({ children, as = 'p', size = 'base', className }: T
 
 	return (
 		<Tag
+			{...(as === 'a' && href ? { href } : {})}
 			className={`
 				text-gray-700
 				leading-relaxed
 				${sizeStyles[size]}
+				
 				${className}
 			`}
 		>
