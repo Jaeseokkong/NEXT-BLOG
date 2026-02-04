1cc2f9e730ac93d26fb94e8498c994e8bb352ddc
jaeseok <wotjr5124@gmail.com>
2026-02-04 16:11:44 +0900

    feat(molecules): PersonalItem

---
+++ src/app/resume/components/molecules/PersonalItem.tsx
@@ -0,0 +1,44 @@
+import Text from '@/components/atoms/Text';
+import Title from '@/components/atoms/Title'
+import Link from 'next/link';
+import { LuExternalLink } from 'react-icons/lu';
+
+interface PersonalItemProps {
+	title: string;
+	description: string;
+	highlights?: string[];
+	link?: string;
+}
+export default function PersonalItem({ title, description, highlights, link }: PersonalItemProps) {
+	return (
+		<div>
+			<div className="flex items-center gap-2">
+				<Title level={4}>{title}</Title>
+			</div>
+
+			{link && (
+				<Link
+					href={link}
+					target="_blank"
+					className="text-gray-500 hover:text-gray-700"
+				>
+					<LuExternalLink size={14} />
+				</Link>
+			)}
+
+			<Text size="sm" className="mt-1">
+				{description}
+			</Text>
+
+			{highlights && (
+				<ul className="mt-2 list-disc list-inside space-y-1">
+					{highlights.map((item, idx) => (
+						<li key={idx}>
+							<Text size="sm">{item}</Text>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	)
+}
