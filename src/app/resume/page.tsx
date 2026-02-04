22531401238d64a88d7890d1d2016f4430d9c328
jaeseok <wotjr5124@gmail.com>
2026-02-04 15:54:00 +0900

    feat(resume): ProjectSection추가

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,6 +2,7 @@ import ResumeHeader from "./components/organisms/ResumeHeader";
 import ResumeTemplate from "./components/templates/ResumeTemplate";
 import SkillSection from "./components/organisms/SkillSection";
 import ExperienceSection from "./components/organisms/ExperienceSection";
+import ProjectSection from "./components/organisms/ProjectSection";
 
 export default function ResumePage() {
 	return (
@@ -9,6 +10,7 @@ export default function ResumePage() {
 			<ResumeHeader />
 			<SkillSection />
 			<ExperienceSection />
+			<ProjectSection />
 		</ResumeTemplate>
 	)
 }
