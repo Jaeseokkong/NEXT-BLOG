7782b744b66d02c5daaee1ff6e971fbc30f04c6f
JaeSeok <wotjr5124@gmail.com>
2026-02-08 23:54:05 +0900

    feat(organisms): ResumeSummary

---
+++ src/app/resume/components/organisms/ResumeSummary.tsx
@@ -0,0 +1,14 @@
+import Text from '@/components/atoms/Text'
+
+export default function ResumeSummary() {
+    return (
+        <section className="mb-8 max-w-3xl">
+            <Text>
+                React기반 실서비스 운영 경험을 가진 프론트엔드 엔지니어입니다.
+                교육 플랫폼과 내부 운영 시스템의 설계·개발 및 UI/UX 개선을 중심으로 작업해왔으며,
+				Next.js와 TypeScript를 활용한 개인 프로젝트 및 대시보드 개발 경험을 보유하고 있습니다.
+                실시간 데이터 시각화와 사용자 흐름 개선을 통해 서비스 품질 향상에 기여해왔습니다.
+            </Text>
+        </section>
+    )
+}
