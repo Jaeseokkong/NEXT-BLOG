848c2ec1e0ca26fcec0300181c60ea1892e62023
JaeSeok <wotjr5124@gmail.com>
2026-02-19 22:52:48 +0900

    feat(sidebar): Title 적용

--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -1,13 +1,15 @@
 import { fetchCategories } from '@/lib/github';
-import SidebarTitle from '../atoms/SidebarTitle';
 import SidebarButton from '../atoms/SidebarButton';
+import Title from '../atoms/Title';
 
 const Sidebar = async () => {
   const categories = await fetchCategories();
 
   return (
     <aside className="hidden md:block w-[220px] shrink-0 p-6 bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 rounded-2xl shadow-sm h-fit sticky top-24">
-      <SidebarTitle />
+      <Title level={2} className=" text-zinc-800 dark:text-zinc-100 mb-4">
+        📚 카테고리
+      </Title>
       <ul className="space-y-3">
         {categories?.map((category) => <SidebarButton key={category} category={category}/>)}
       </ul>
