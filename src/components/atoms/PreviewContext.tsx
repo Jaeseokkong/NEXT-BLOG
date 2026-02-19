7fd8420db224e84c9fc353ed770878910b0f67c0
JaeSeok <wotjr5124@gmail.com>
2026-02-19 22:51:18 +0900

    feat(post): Section 내부 title, text 변경

--- src/components/atoms/PreviewContext.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PreviewContext({ context }: { context: string; }) {
-    return (
-        <p className="text-zinc-700 dark:text-zinc-300 mb-3 whitespace-pre-line">
-            {context}
-        </p>
-    )
-}
--- src/components/organisms/PreviewSection.tsx
+++ src/components/organisms/PreviewSection.tsx
@@ -1,6 +1,6 @@
-import PreviewTitle from "../atoms/PreviewTitle";
-import PreviewContext from "../atoms/PreviewContext";
 import PreviewMoreButton from "../atoms/PreviewMoreButton";
+import Title from "../atoms/Title";
+import Text from "../atoms/Text";
 
 interface PreviewSectionProps {
   title: string;
@@ -20,8 +20,14 @@ export default function PreviewSection({
 }: PreviewSectionProps) {
   return (
     <section className="bg-zinc-100 dark:bg-zinc-800 rounded-xl p-6 shadow-sm">
-      <PreviewTitle title={title} />
-      {context && <PreviewContext context={context} />}
+      <Title level={2} className="mb-2 text-zinc-800 dark:text-zinc-100">
+        {title}
+      </Title>
+      {context && 
+        <Text as="p" className="text-zinc-700 dark:text-zinc-300 mb-3 whitespace-pre-line">
+          {context}
+        </Text>
+      }
       {children}
       {moreButton && <PreviewMoreButton href={moreButton.href} label={moreButton.label} />}
     </section>
