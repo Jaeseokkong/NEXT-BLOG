ef301d1787b6ec5efb22df1944475e9e74245171
jaeseok <wotjr5124@gmail.com>
2026-02-24 17:32:21 +0900

    feat(atoms): Text 사이즈 추가

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -3,7 +3,7 @@ import React, { JSX } from 'react'
 interface TextProps {
 	children: React.ReactNode;
 	as?: 'p' | 'span' | 'div' | 'a';
-	size?: 'sm' | 'base' | 'lg';
+	size?: 'xs' | 'sm' | 'base' | 'lg';
 	href?: string;
 	className?: string;
 }
@@ -12,6 +12,7 @@ export default function Text({ children, as = 'p', size = 'base', href, classNam
 	const Tag = as as keyof JSX.IntrinsicElements;
 
 	const sizeStyles = {
+		xs: 'text-xs',
 		sm: 'text-sm',
 		base: 'text-base',
 		lg: 'text-lg'
