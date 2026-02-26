78314f644864ba9a41c3b1b1b436b01d768da738
JaeSeok <wotjr5124@gmail.com>
2026-02-26 21:44:58 +0900

    refactor(preview): PreviewPostCard 공통 컴포넌트 재사용

--- src/components/molecules/PreviewPostCard.tsx
+++ src/components/molecules/PreviewPostCard.tsx
@@ -1,10 +1,9 @@
 import { PostMeta } from '@/lib/github'
 import Link from 'next/link'
 import React from 'react'
-import PostPreviewCardTop from '../atoms/PostPreviewCardTop'
-import PostPreviewCardTitle from '../atoms/PostPreviewCardTitle'
-import PostPreviewCardContext from '../atoms/PostPreviewCardContext'
-import PostPreviewCardMoreButton from '../atoms/PostPreviewCardMoreButton'
+import Text from '../atoms/Text'
+import Title from '../atoms/Title'
+import MoreButton from '../atoms/MoreButton'
 
 export default function PreviewPostCard({ post }: { post : PostMeta }) {
     return (
@@ -15,10 +14,13 @@ export default function PreviewPostCard({ post }: { post : PostMeta }) {
                 className="group block rounded-2xl border border-zinc-200 dark:border-zinc-700 bg-white dark:bg-zinc-800 shadow-sm hover:shadow-md transition-all p-5 hover:-translate-y-1"
             >
                 <div className="flex flex-col h-full">
-                    <PostPreviewCardTop category={post.category} date={post.date}/>
-                    <PostPreviewCardTitle title={post.title}/>
-                    {post.excerpt && <PostPreviewCardContext excerpt={post.excerpt} />}
-                    <PostPreviewCardMoreButton />
+                    <div className="flex items-center justify-between mb-2">
+                        <Text as="span" size="xs" className="text-indigo-500 dark:text-indigo-400 uppercase tracking-wide">{post.category}</Text>
+                        <Text as="span" size="xs" className="text-zinc-400">📅 {post.date}</Text>
+                    </div>
+                    <Title level={2}>{post.title}</Title>
+                    {post.excerpt && <Text size="sm" className="text-zinc-600 dark:text-zinc-400 mt-2 line-clamp-2">{post.excerpt}</Text>}
+                    <MoreButton>더 보기</MoreButton>
                 </div>
             </Link>
         </li>
