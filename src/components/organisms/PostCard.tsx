ae76d882f3f7e430d7e184fd535a9d1319c74113
JaeSeok <wotjr5124@gmail.com>
2026-03-02 22:16:57 +0900

    refactor(post): PostCardBottom 제거 및 PostCard 내부로 통합

--- src/components/organisms/PostCard.tsx
+++ src/components/organisms/PostCard.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Thumbnail from "../atoms/Thumbnail";
-import PostCardBottom from "../molecules/PostCardInfo";
+import Text from "../atoms/Text";
+import Title from "../atoms/Title";
+import MoreButton from "../atoms/MoreButton";
 
 interface PostCardProps {
   title: string;
@@ -20,7 +22,14 @@ const PostCard = ({ post }: { post: PostCardProps }) => {
       bg-white dark:bg-zinc-900 shadow-sm transition-all duration-300 hover:-translate-y-1 hover:shadow-lg"
     >
       <Thumbnail image={post.image} title={post.title} category={post.category} />
-      <PostCardBottom category={post.category} title={post.title} date={post.date} />
+      <div className="flex flex-col p-4 flex-1">
+          <Text as="span" size="xs" className="text-indigo-500 dark:text-indigo-400 font-medium uppercase tracking-wide mb-1">{post.category} </Text>
+          <Title>{post.title}</Title>
+          <div className="mt-auto pt-4 flex justify-between items-center text-xs text-zinc-400">
+              <span>📅 {post.date}</span>
+              <MoreButton>더 보기</MoreButton>
+          </div>
+      </div>
     </Link>
   );
 };
