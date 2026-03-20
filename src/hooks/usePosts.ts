0cd7db0db384580a96214dbe646ca7268a4c1d09
JaeSeok <wotjr5124@gmail.com>
2026-03-20 17:26:35 +0900

    fix(posts): usePosts 훅 개선

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,13 +1,11 @@
-import { PostMeta } from "@/types/post";
 import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "@/lib/api/posts";
+import { PostResponse } from "@/types/post";
 
-
-export function usePosts(): PostMeta[] {
-    const { data } = useQuery({
-        queryKey: ["posts"],
-        queryFn: () => {},
-    })
-
-    return data;
+export function usePosts(page: number = 1, category?: string) {
+  return useQuery<PostResponse>({
+    queryKey: ["posts", page, category],
+    queryFn: () => fetchPosts(page, category),
+    staleTime: 1000 * 60 * 5
+  });
 }
-
