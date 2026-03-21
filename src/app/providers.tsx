66b3bf439ed11591320b8b000c33322509c3b1a3
JaeSeok <wotjr5124@gmail.com>
2026-03-21 21:39:12 +0900

    feat(query): Provider tanstack query provider 적용

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import { queryClient } from '@/lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={false}>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem={false}>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
