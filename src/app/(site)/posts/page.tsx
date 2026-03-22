1c3ab63621052c94a6ed971665e44d288d80eced
JaeSeok <wotjr5124@gmail.com>
2026-03-22 22:18:04 +0900

    feat:(posts): usePosts 적용

--- src/app/(site)/posts/page.tsx
+++ src/app/(site)/posts/page.tsx
@@ -3,20 +3,15 @@ import MobileCategoryTabs from "@/components/posts/MobileCategoryTabs";
 import PostsContainer from "@/components/posts/PostsContainer";
 import ScrollToTopButton from "@/components/posts/ScrollToTopButton";
 import Sidebar from "@/components/molecules/Sidebar";
-import { fetchPosts } from "@/lib/github";
 
-
-export default async function PostsPage() {
-  const { posts: initialPosts } = await fetchPosts(1);
-  
-
+export default async function PostsPage() {  
   return (
     <main className="max-w-7xl mx-auto px-4 py-8 md:flex gap-6">
       <Sidebar />
       <div className="flex-1 flex flex-col gap-6">
         <Intro />
         <MobileCategoryTabs />
-        <PostsContainer initialPosts={initialPosts}/>
+        <PostsContainer />
       </div>
       <ScrollToTopButton />
     </main>
--- src/components/posts/PostsContainer.tsx
+++ src/components/posts/PostsContainer.tsx
@@ -4,14 +4,13 @@ import { useState, useEffect } from "react";
 import SearchInput from "@/components/posts/SearchInput";
 import PostList from "@/components/posts/PostList";
 import { PostMeta } from "@/lib/github";
+import { usePosts } from "@/hooks/usePosts";
 
-type PostsContainerProps = {
-  initialPosts: PostMeta[];
-};
 
-const PostsContainer = ({ initialPosts }: PostsContainerProps) => {
+const PostsContainer = () => {
   const [searchInput, setSearchInput] = useState('');
   const [debouncedKeyword, setDebouncedKeyword] = useState('');
+  const { data, isLoading } = usePosts(1);
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -21,14 +20,17 @@ const PostsContainer = ({ initialPosts }: PostsContainerProps) => {
     return () => clearTimeout(handler);
   }, [searchInput]);
 
+  if (isLoading) {
+    return <div>Loading..</div>
+  }
 
   return (
     <div className="flex flex-col gap-3">
       <SearchInput onSearch={setSearchInput} placeholder="검색어를 입력하세요..." />        
-      {initialPosts.length === 0 ? 
+      {data.posts.length === 0 ? 
         <p className="text-center text-gray-500">검색 결과가 없습니다.</p>
        : 
-        <PostList initialPosts={initialPosts} searchKeyword={debouncedKeyword} />
+        <PostList initialPosts={data.posts} searchKeyword={debouncedKeyword} />
       }
     </div>
   );
