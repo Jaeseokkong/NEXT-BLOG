2e2349b39f22b4d73fee4a02b472f0123344c7f6
JaeSeok <wotjr5124@gmail.com>
2026-03-22 22:30:17 +0900

    feat(posts): fetch url env 적용

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -9,7 +9,7 @@ export async function fetchPosts(page: number = 1, category?: string): Promise<P
     query.append("category", category);
   }
 
-  const res = await fetch(`/api/posts?${query.toString()}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/posts?${query.toString()}`);
 
   if (!res.ok) {
     throw new Error("게시글 데이터를 불러오지 못했습니다.");
