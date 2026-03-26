13da44efc99af06ddfd5dc8d95ba74de832ebe73
JaeSeok <wotjr5124@gmail.com>
2026-03-26 17:37:07 +0900

    feat(posts): all 및 cateogory posts 요청 함수 통합

--- src/lib/api/posts.ts
+++ src/lib/api/posts.ts
@@ -1,29 +1,12 @@
 import { PostResponse } from "@/types/post";
 
-export async function fetchPosts({ page = 1 }: { page: number }): Promise<PostResponse> {
-  const query = new URLSearchParams({
-    page: page.toString(),
-  });
+export async function fetchPosts({ page = 1, category }: { page: number, category?: string } ): Promise<PostResponse> {
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/posts?${query.toString()}`);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/${category ? category : ""}`);
 
   if (!res.ok) {
     throw new Error("게시글 데이터를 불러오지 못했습니다.");
-  }
-
-  return res.json();
-}
-
-export async function fetchPostsByCategory({ page = 1, category }: { page: number, category: string }): Promise<PostResponse> {
-  const query = new URLSearchParams({
-    page: page.toString(),
-    category: category
-  })
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/posts?${query.toString()}`);
-
-  if (!res.ok) {
-    throw new Error(`${category}의 게시글 데이터를 불러오지 못했습니다.`);
   }
 
   return res.json();
