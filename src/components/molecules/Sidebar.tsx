7e3146344661648c04171248b26c70420762f831
JaeSeok <wotjr5124@gmail.com>
2026-03-31 22:48:25 +0900

    feat(layout): Sidebar Category fetch 제거 및 Map으로 카테고리 설정

--- src/components/molecules/Sidebar.tsx
+++ src/components/molecules/Sidebar.tsx
@@ -1,10 +1,10 @@
-import { fetchCategories } from '@/lib/github';
+import { CATEGORY_MAP, MainCategory } from '@/constants/category';
 import SidebarButton from '../atoms/SidebarButton';
 import Title from '../atoms/Title';
 
 const Sidebar = async () => {
-  const categories = await fetchCategories();
-
+  const categories = Object.keys(CATEGORY_MAP) as MainCategory[];
+  
   return (
     <aside className="hidden md:block w-[220px] shrink-0 p-6 bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 rounded-2xl shadow-sm h-fit sticky top-24">
       <Title level={2} className=" text-zinc-800 dark:text-zinc-100 mb-4">
