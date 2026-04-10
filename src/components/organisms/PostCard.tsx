4f470a35c56b95ee93ec5b59a5762908f5c32385
JaeSeok <wotjr5124@gmail.com>
2026-04-10 17:30:19 +0900

    refactor(post): PostCard props type PostMeta로 지정 및 임시 주석 처리

--- src/components/organisms/PostCard.tsx
+++ src/components/organisms/PostCard.tsx
@@ -3,19 +3,10 @@ import Thumbnail from "../atoms/Thumbnail";
 import Text from "../atoms/Text";
 import Title from "../atoms/Title";
 import MoreButton from "../atoms/MoreButton";
-import { MainCategory } from "@/constants/category";
+import { PostMeta } from "@/types/post";
 
-interface PostCardProps {
-  title: string;
-  date: string;
-  slug: string;
-  category: MainCategory;
-  excerpt?: string;
-  image?: string;
-  path: string;
-}
 
-const PostCard = ({ post }: { post: PostCardProps }) => {
+const PostCard = ({ post }: { post: PostMeta }) => {
 
   return (
     <Link
@@ -23,12 +14,12 @@ const PostCard = ({ post }: { post: PostCardProps }) => {
       className="group flex flex-col overflow-hidden rounded-2xl border border-zinc-200 dark:border-zinc-700 
       bg-white dark:bg-zinc-900 shadow-sm transition-all duration-300 hover:-translate-y-1 hover:shadow-lg"
     >
-      <Thumbnail image={post.image} title={post.title} category={post.category} />
+      {/* <Thumbnail image={post.image} title={post.name} category={post.category} /> */}
       <div className="flex flex-col p-4 flex-1">
           <Text as="span" size="xs" className="text-indigo-500 dark:text-indigo-400 font-medium uppercase tracking-wide mb-1">{post.category} </Text>
           <Title>{post.title}</Title>
           <div className="mt-auto pt-4 flex justify-between items-center text-xs text-zinc-400">
-              <span>📅 {post.date}</span>
+              {/* <span>📅 {post.date}</span> */}
               <MoreButton>더 보기</MoreButton>
           </div>
       </div>
