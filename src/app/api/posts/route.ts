11744aae6768fe1e9e6182927209ac0bfe99f47d
JaeSeok <wotjr5124@gmail.com>
2026-04-11 12:11:00 +0900

    refactor(route): tree length 변수 제거 및 조건 부여

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,3 +1,4 @@
+import { MainCategory } from "@/constants/category";
 import { getAllPostPaths } from "@/lib/github";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,12 +7,11 @@ const PAGE_SIZE = 12;
 export async function GET(req: NextRequest) {
   const pageParam = req.nextUrl.searchParams.get("page");
   const search = req.nextUrl.searchParams.get("search")?.toLowerCase() || '';
-  const category = req.nextUrl.searchParams.get("category");
+  const category = req.nextUrl.searchParams.get("category") as MainCategory;
   const page = pageParam ? parseInt(pageParam, 10) : 1;
   let more = false;
 
   let trees = await getAllPostPaths();
-  let n = trees.length
 
   // 카테고리 필터링
   if (category) {
@@ -33,11 +33,11 @@ export async function GET(req: NextRequest) {
 
   // 최신순으로 정렬
   trees.sort((a, b) => getDate(b.path) - getDate(a.path));
-
+  
   const start = (page - 1) * PAGE_SIZE;
   const selectedFiles = trees.slice(start, start + PAGE_SIZE + 1);
 
-  if (n > start + PAGE_SIZE + 1) more = true;
+  if (trees.length > start + PAGE_SIZE + 1) more = true;
 
   return NextResponse.json({
     posts: selectedFiles,
