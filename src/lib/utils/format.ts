6d32b962aeae638f4202097cc4a92ff81563bad4
JaeSeok <wotjr5124@gmail.com>
2026-04-12 21:51:18 +0900

    feat(format): path 에서 파일 관련 정도 파싱 포맷 함수 추가

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -1,14 +1,16 @@
-export function formatDate(dateString: string): string {
-  const d = new Date(
-    dateString.slice(0, 4) +
-      '-' +
-      dateString.slice(4, 6) +
-      '-' +
-      dateString.slice(6)
-  );
-  return d.toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+export const parsePostFile = (path: string) => {
+  const paths = path.split("/");
+  const fileName = paths[path.length - 1];
+
+  const nameWithoutExt = fileName.replace(".md", "");
+
+  const [year, month, day, ...titleParts] = nameWithoutExt.split("-");
+  const title = titleParts.join("-");
+
+  const date = `${year}-${month}-${day}`;
+
+  return {
+    title,
+    date
+  }
 }
