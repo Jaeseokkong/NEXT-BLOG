5606652b14ba54610b594b4fb372ad93e1eec38d
jaeseok <wotjr5124@gmail.com>
2026-04-13 15:58:28 +0900

    chore(eslint): type module로 수정 및 eslint 파일 수정

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import nextPlugin from "@next/eslint-plugin-next";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs["core-web-vitals"].rules
+    }
+  }
+];
