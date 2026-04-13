2b8948d032b14039e7df46401143df29ba983af2
jaeseok <wotjr5124@gmail.com>
2026-04-13 16:22:21 +0900

    test(query): customRender으로 수정

--- src/test-utils/index.tsx
+++ src/test-utils/index.tsx
@@ -1,21 +1,25 @@
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactNode } from 'react';
+import { ReactElement } from 'react';
 
-function createWrapper() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-    },
-  })
+const generateQueryClient = () => {
+	return new QueryClient({
+		defaultOptions: {
+			queries: {
+				retry: false,
+			},
+		},
+	})
+};
 
-  return ({ children }: { children: ReactNode }) => (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  )
+const customRender = (ui: ReactElement, client?: QueryClient) => {
+	const queryClient = client ?? generateQueryClient();
+
+	return render(
+		<QueryClientProvider client={queryClient}>
+			{ui}
+		</QueryClientProvider>
+	);
 }
 
-export { createWrapper as render };
+export { customRender as render };
