b0c6df9ccd9fc5dc7292d5060f6a6580067f8521
jaeseok <wotjr5124@gmail.com>
2026-04-19 22:44:12 +0900

    feat(category): All 카테고리 선택 시 쿼리스트링 제거 로직 및 Category 타입(ALL 포함) 추가

--- src/components/atoms/SidebarButton.tsx
+++ src/components/atoms/SidebarButton.tsx
@@ -4,12 +4,17 @@ import { MainCategory } from '@/constants/category';
 import { useRouter } from 'next/navigation';
 import React from 'react'
 
-export default function SidebarButton({ category }: { category: MainCategory }) {
+type Category = MainCategory | "ALL";
+
+export default function SidebarButton({ category }: { category: Category }) {
 	const router = useRouter();
 	
-	const handleClick = (category: MainCategory) => {
-		console.log(category)
-		router.push(`posts?category=${category}`)
+	const handleClick = () => {
+		if (category === "ALL") {
+			router.push('posts')
+		} else {
+			router.push(`posts?category=${category}`)
+		}
 	}
 
 	return (
@@ -17,7 +22,7 @@ export default function SidebarButton({ category }: { category: MainCategory }) 
 			className="block px-4 py-2 rounded-lg bg-zinc-100 w-full text-left cursor-pointer
 					dark:bg-zinc-800 hover:bg-indigo-100 dark:hover:bg-indigo-300/10 text-zinc-800 
 					dark:text-zinc-100 font-medium transition-colors duration-200"
-			key={category} onClick={() => handleClick(category)}>
+			key={category} onClick={handleClick}>
 			{category}
 		</li>
 	)
