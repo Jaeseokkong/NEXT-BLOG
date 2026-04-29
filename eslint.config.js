50979c09a7bb24dbf490480c10a889cc0b7ed07d
jaeseok <wotjr5124@gmail.com>
2026-04-29 20:18:43 +0900

    chore(config): eslint unused-imports 플러그인 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -9,10 +9,22 @@ export default [
   {
     plugins: {
       "@next/next": nextPlugin,
+      "unused-imports": unusedImports
     },
     rules: {
       ...nextPlugin.configs["core-web-vitals"].rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ]
     }
   }
 ];
