659b9566dcb72d90f5defc44d08da98bfbde4d13
jaeseok <wotjr5124@gmail.com>
2026-05-01 09:27:33 +0900

    refactor(posts): useInfinitePost 쿼리 적용 및 검색 결과 없을 경우 처리 변경

--- src/components/posts/PostsContainer.tsx
+++ src/components/posts/PostsContainer.tsx
@@ -4,26 +4,29 @@ import { useState, useEffect } from "react";
 import SearchInput from "@/components/posts/SearchInput";
 import PostList from "@/components/posts/PostList";
 import { usePosts } from "@/hooks/usePosts";
-import { PostResponse } from "@/types/post";
 import { MainCategory } from "@/constants/category";
 import {  useSearchParams } from "next/navigation";
+import { useInfinitePosts } from "@/hooks/useInfinitePosts";
 
 
 const PostsContainer = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
   const searchParams = useSearchParams();
   const [category, setCategory] = useState<MainCategory | undefined>(undefined);
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = usePosts(category, searchKeyword);
-  const posts = data?.pages.flatMap((page) => page.posts) ?? [];
-
+  const { posts, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfinitePosts(category, searchKeyword);
+  
   useEffect(() => {
     setCategory(searchParams.get("category") as MainCategory);
   }, [searchParams])
 
   return (
     <div className="flex flex-col gap-3">
+      <SearchInput onSearch={setSearchKeyword}/>
       {posts.length === 0 ? 
-        <p className="text-center text-gray-500">검색 결과가 없습니다.</p>
+        searchKeyword ?
+          <p className="text-center text-gray-500">검색 결과가 없습니다.</p> 
+          :
+          <p className="text-center text-gray-500">결과가 없습니다.</p>
        : 
         <PostList posts={posts} fetchNextPage={fetchNextPage} hasNextPage={hasNextPage} isFetchingNextPage={isFetchingNextPage} />
       }
