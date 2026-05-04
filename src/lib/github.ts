6561bfcc9cdc7ed7a909a97504be1d96fa81ecbc
jaeseok <wotjr5124@gmail.com>
2026-05-04 14:45:17 +0900

    fix(git): fetch 에러 관리 코드 삽입

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -3,7 +3,7 @@ import { RepoTreeItem } from "@/types/post";
 const GITHUB_RAW_BASE_URL = "https://raw.githubusercontent.com/Jaeseokkong/TIL/main";
 
 const headers = {
-  "Accept": "application/vnd.github.v3+json",
+  "Accept": "application/vnd.github+json",
   "User-Agent": "Next-TIL-App",
   "Authorization": `token ${process.env.GITHUB_TOKEN}`
 };
@@ -71,7 +71,6 @@ export async function fetchPosts(
 
 let cachedPosts: PostItemType[] = [];
 let lastFetched = 0;
-
 const CACHE_TTL = 1000 * 60 * 60; // 1시간
 
 export async function getAllPosts(): Promise<PostItemType[]> {
@@ -81,10 +80,18 @@ export async function getAllPosts(): Promise<PostItemType[]> {
     return cachedPosts;
   }
 
+  if (!process.env.GITHUB_TREE_BASE_URL) {
+    throw new Error("Missing GITHUB_TREE_BASE_URL");
+  }
+
   const res = await fetch(`${process.env.GITHUB_TREE_BASE_URL}?recursive=1`, {
       headers,
       next: { revalidate: 3600 }
   });
+
+  if (!res.ok) {
+    throw new Error("GitHub API fetch failed");
+  }
 
   const data = await res.json();
 
