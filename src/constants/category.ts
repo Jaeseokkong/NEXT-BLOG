c0a8e759d1003214d6f370f013704e42b5af1c56
Jaeseokkong <wotjr5124@gmail.com>
2026-05-07 22:28:57 +0900

    refactor(category): 카테고리 순서 및 추가 - 프론트엔드 위주 상위 카테고리로 편성 - TroubleShooting 카테고리 추가

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,24 +1,25 @@
 export const CATEGORY_MAP = {
-    Algorithm: [],
-    CSS: [],
-    JWT: [],
-    JavaScript: [],
-    "Next.js": [],
-    Nginx: [],
-    Node: [],
-    Performance: [],
-    React: [
-      "Architecture",
-      "Core",
-      "Form",
-      "Integration",
-      "State-Management",
-      "TanStack-Query",
-    ],
-    Spring: [],
-    Testing: [],
-    TypeScript: [],
-    Web3: [],
+  JavaScript: [],
+  TypeScript: [],
+  React: [
+    "Core",
+    "State-Management",
+    "Architecture",
+    "TanStack-Query",
+    "Form",
+    "Integration",
+  ],
+  "Next.js": [],
+  TroubleShooting: [],
+  Testing: [],
+  Performance: [],
+  CSS: [],
+  Node: [],
+  JWT: [],
+  Web3: [],
+  Nginx: [],
+  Algorithm: [],
+  Spring: [],
 } as const;
 
 export type MainCategory = keyof typeof CATEGORY_MAP;
@@ -26,17 +27,24 @@ export type MainCategory = keyof typeof CATEGORY_MAP;
 export type Category = MainCategory | "ALL";
 
 export const CATEGORY_BG_COLORS: Record<MainCategory, string> = {
-    Algorithm: "bg-gray-100",
-    CSS: "bg-indigo-100",
-    JWT: "bg-amber-100",
-    JavaScript: "bg-yellow-100",
-    "Next.js": "bg-zinc-200",
-    Nginx: "bg-emerald-100",
-    Node: "bg-green-200",
-    Performance: "bg-red-100",
-    React: "bg-sky-100",
-    Spring: "bg-lime-200",
-    Testing: "bg-orange-100",
-    TypeScript: "bg-blue-100",
-    Web3: "bg-purple-100",
+  JavaScript: "bg-yellow-100",
+  TypeScript: "bg-blue-100",
+
+  React: "bg-cyan-100",
+  "Next.js": "bg-neutral-200",
+
+  TroubleShooting: "bg-rose-100",
+
+  CSS: "bg-indigo-100",
+  Performance: "bg-red-100",
+  Testing: "bg-orange-100",
+
+  Node: "bg-green-100",
+  JWT: "bg-amber-100",
+
+  Web3: "bg-violet-100",
+  Nginx: "bg-emerald-100",
+
+  Algorithm: "bg-slate-200",
+  Spring: "bg-lime-100",
 };
