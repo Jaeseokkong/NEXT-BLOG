2da80e10c26465fc570c2dcd7a19069dd00eb263
Jaeseokkong <wotjr5124@gmail.com>
2026-05-07 22:38:00 +0900

    refactor(github): 사용하지 않는 함수 제거 (fetchPosts)

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -23,31 +23,6 @@ export async function fetchMarkdownFileByPath(path: string): Promise<string> {
   return content;
 }
 
-export async function fetchPosts(
-  page: number = 1,
-  category?: string
-): Promise<PostResponse> {
-
-  const query = new URLSearchParams({
-    page: page.toString(),
-  });
-
-  if (category) {
-    query.append("category", category);
-  }
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/posts?${query.toString()}`,
-    {
-      next: { revalidate: 300 },
-    }
-  );
-
-  if (!res.ok) throw new Error("게시글 데이터를 불러오지 못했습니다.");
-
-  return res.json();
-}
-
 let cachedPosts: PostItem[] = [];
 let lastFetched = 0;
 const CACHE_TTL = 1000 * 60 * 60; // 1시간
