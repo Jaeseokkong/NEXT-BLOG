c3a065431226525acc950181a9463e1c417c89d4
JaeSeok <wotjr5124@gmail.com>
2026-05-15 20:18:43 +0900

    chore(about): about preview 컴포넌트 about 전용 컴포넌트 폴더로 이동 및 이름 수정

---
+++ src/app/(site)/about/_components/Preview.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import PreviewSection from '../../../../components/organisms/PreviewSection';
+import BadgeGroup from '../../../../components/molecules/BadgeGroup';
+
+const techList: string[] = [ "JavaScript", "TypeScript", "React", "Next.js" ];
+
+export default function AboutPreview() {
+  return (
+    <PreviewSection 
+      title="🙋‍♂️ About Me"
+      context={`프론트엔드 개발자이며, 사용자 경험과 성능에 집중하여 웹 서비스를 만듭니다.\nReact와 Next.js를 주로 사용하며, 사이드 프로젝트와 기술 블로그 작성에도 열정이 있습니다.`}
+      moreButton={{ href:"/about", label: "프로필 더보기"}}
+    >
+      <BadgeGroup labels={techList} />
+    </PreviewSection>
+  );
+}
