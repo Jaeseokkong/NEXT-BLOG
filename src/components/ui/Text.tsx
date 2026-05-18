057a83036c6cf1a4f564fc0b90cef49fece53de3
jaeseok <wotjr5124@gmail.com>
2026-05-19 08:41:32 +0900

    refactor(ui): 공통 텍스트 컴포넌트 컬러 스타일 및 color props 추가

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,22 +1,30 @@
 import React, { JSX } from 'react'
 
+const colorStyles = {
+	primary: "text-zinc-800 dark:text-zinc-200",
+	secondary: "text-zinc-500 dark:text-zinc-400",
+	muted: "text-zinc-400 dark:text-zinc-500",
+}
+
+const sizeStyles = {
+	xs: 'text-xs',
+	sm: 'text-sm',
+	base: 'text-base',
+	lg: 'text-lg'
+}
+
 interface TextProps {
 	children: React.ReactNode;
 	as?: 'p' | 'span' | 'div' | 'a';
 	size?: 'xs' | 'sm' | 'base' | 'lg';
 	href?: string;
 	className?: string;
+	color: keyof typeof colorStyles;
 }
 
-export default function Text({ children, as = 'p', size = 'base', href, className }: TextProps) {
+export default function Text({ children, as = 'p', size = 'base', href, className, color = "primary" }: TextProps) {
 	const Tag = as as keyof JSX.IntrinsicElements;
 
-	const sizeStyles = {
-		xs: 'text-xs',
-		sm: 'text-sm',
-		base: 'text-base',
-		lg: 'text-lg'
-	}
 
 	return (
 		<Tag
