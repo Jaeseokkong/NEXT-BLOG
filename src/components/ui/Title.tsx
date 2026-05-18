2a778bf1e19656f3da11d25c3a25c621e001c326
jaeseok <wotjr5124@gmail.com>
2026-05-19 08:40:39 +0900

    refactor(ui): 공통 타이틀 컴포넌트 스타일별 컬러 스타일 추가

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -10,11 +10,11 @@ export default function Title({ level = 3, children, className = ""}: TitleProps
 	const Tag = `h${level}` as const;
 
 	const style = {
-		1: "text-3xl font-bold",
-		2: "text-xl font-semibold",
-		3: "text-base font-semibold",
-		4: "text-sm font-medium",
-		5: "text-xs font-medium uppercase traking-wide"
+		1: "text-3xl font-bold text-zinc-900 dark:text-zinc-100",
+  		2: "text-xl font-semibold text-zinc-900 dark:text-zinc-100",
+  		3: "text-base font-semibold text-zinc-800 dark:text-zinc-200",
+  		4: "text-sm font-medium text-zinc-700 dark:text-zinc-300",
+  		5: "text-xs font-medium uppercase tracking-wide text-zinc-500 dark:text-zinc-400",
 	}
 
 	return (
