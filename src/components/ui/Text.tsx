2f9ea262d9b64b67a6dc9f531ea03ff01c8e7a61
jaeseok <wotjr5124@gmail.com>
2026-06-03 20:18:43 +0900

    refactor(ui): Text 컴포넌트 중복 제거 및 ui 컴포넌트로 수정

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,29 +1,29 @@
 import React, { JSX } from 'react'
 
-const colorStyles = {
-	primary: "text-zinc-800 dark:text-zinc-200",
-	secondary: "text-zinc-500 dark:text-zinc-400",
-	muted: "text-zinc-400 dark:text-zinc-500",
-}
-
-const sizeStyles = {
-	xs: 'text-xs',
-	sm: 'text-sm',
-	base: 'text-base',
-	lg: 'text-lg'
-}
-
 interface TextProps {
 	children: React.ReactNode;
 	as?: 'p' | 'span' | 'div' | 'a';
 	size?: 'xs' | 'sm' | 'base' | 'lg';
+	color: 'primary' | 'secondary' | 'muted';
 	href?: string;
 	className?: string;
-	color?: keyof typeof colorStyles;
 }
 
-export default function Text({ children, as = 'p', size = 'base', href, className, color = "primary" }: TextProps) {
+export default function Text({ children, as = 'p', size = 'base', color = 'primary', href, className }: TextProps) {
 	const Tag = as as keyof JSX.IntrinsicElements;
+
+	const sizeStyles = {
+		xs: 'text-xs',
+		sm: 'text-sm',
+		base: 'text-base',
+		lg: 'text-lg'
+	}
+
+	const colorStyles = {
+		primary: 'text-zinc-800 dark:text-zinc-200',
+		secondary: 'text-zinc-600 dark:text-zinc-400',
+		muted: 'text-zinc-500 dark:text-zinc-500'
+	}
 
 	return (
 		<Tag
