db2cfd1cfbb79dc1d84a73301406753b222db78f
jaeseok <wotjr5124@gmail.com>
2026-06-17 15:42:46 +0900

    fix(all): 전체 Text 컴포넌트 props 누란된 내용 수정

--- src/app/resume/components/organisms/ResumeSummary.tsx
+++ src/app/resume/components/organisms/ResumeSummary.tsx
@@ -3,7 +3,7 @@ import Text from '@/components/ui/Text'
 export default function ResumeSummary() {
     return (
         <section className="mb-8 max-w-3xl">
-            <Text size="sm">
+            <Text color="secondary" size="sm">
                 React 기반 실서비스 운영 경험을 보유한 프론트엔드 개발자입니다.<br/>
                 교육 플랫폼 및 관리자 대시보드를 개발하며 데이터 시각화와 사용자 경험 개선에 집중해왔습니다.<br/>
 				Next.js와 TypeScript 기반 프로젝트를 진행하며 컴포넌트 구조 설계와 유지보수성 향상을 경험했습니다.<br/>
