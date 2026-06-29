0e2b97e008e2c756ff3cc66144e13187e6359582
jaeseok <wotjr5124@gmail.com>
2026-06-29 16:08:02 +0900

    style(portfolio): 컨텐츠 레이아웃 섹션 main 및 스타일 적용

--- src/app/(site)/portfolio/page.tsx
+++ src/app/(site)/portfolio/page.tsx
@@ -5,11 +5,11 @@ import ProjectsSection from "./_components/ProjectsSection";
 
 export default async function PortfolioPage() {  
     return(
-        <>
+        <main className="max-w-4xl mx-auto px-4 py-8 gap-6">
             <Hero/>
             <SkillsSection />
             <CareerSection />
             <ProjectsSection />
-        </>
+        </main>
     );
 }
