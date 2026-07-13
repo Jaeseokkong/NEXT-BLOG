8a38ac64469d9399c4fef652f458516a1fe7c2e2
jaeseok <wotjr5124@gmail.com>
2026-07-14 08:35:40 +0900

    refactor(post): 포스트 검색 관련 표시 Text 컴포넌트 적용

--- src/app/(site)/posts/_components/PostsContainer.tsx
+++ src/app/(site)/posts/_components/PostsContainer.tsx
@@ -4,11 +4,11 @@ import { useState, useEffect } from "react";
 import SearchInput from "@/app/(site)/posts/_components/SearchInput";
 import PostList from "@/app/(site)/posts/_components/PostList";
 import { MainCategory } from "@/constants/category";
-import {  useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useInfinitePosts } from "@/hooks/useInfinitePosts";
 import CategoryFilter from "./CategoryFilter";
 import Title from "@/components/ui/Title";
-
+import Text from "@/components/ui/Text";
 
 const PostsContainer = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -20,24 +20,27 @@ const PostsContainer = () => {
 
   useEffect(() => {
     const param = searchParams.get("category");
-    setCategory(param ? (param as MainCategory) : undefined)
-  }, [searchParams])
+    setCategory(param ? (param as MainCategory) : undefined);
+  }, [searchParams]);
 
   return (
-    <div className="flex flex-col gap-3">
-      <Title>Posts</Title>
+    <div className="flex flex-col gap-4">
+      <Title level={1}>Posts</Title>
       <CategoryFilter />
       <SearchInput onSearch={setSearchKeyword} />
       {isFetching && isEmpty ? (
-        <p className="text-center text-gray-400">검색 중...</p>
+        <Text color="muted" className="text-center">검색 중...</Text>
       ) : isEmpty ? (
-            isSearching ? (
-              <p className="text-center text-gray-500">검색 결과가 없습니다.</p>
-            ) : (
-              <p className="text-center text-gray-500">결과가 없습니다.</p>
-            )
+        <Text color="muted" className="text-center">
+          {isSearching ? "검색 결과가 없습니다." : "결과가 없습니다."}
+        </Text>
       ) : (
-        <PostList posts={posts} fetchNextPage={fetchNextPage} hasNextPage={hasNextPage} isFetchingNextPage={isFetchingNextPage} />
+        <PostList
+          posts={posts}
+          fetchNextPage={fetchNextPage}
+          hasNextPage={hasNextPage}
+          isFetchingNextPage={isFetchingNextPage}
+        />
       )}
     </div>
   );
